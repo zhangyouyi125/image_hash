16a0747edc8f8496eb92101b676b42e3eae9e9e8
zhangyouyi125 <1197902754@qq.com>
2022-12-04 22:45:25 +0800

    first commit

---
+++ ahash.py
@@ -0,0 +1,6 @@
+'''
+Author: zyy
+Date: 2022-12-04 22:42:02
+LastEditTime: 2022-12-04 22:42:04
+Description: 
+'''
---
+++ phash.py
@@ -0,0 +1,6 @@
+'''
+Author: zyy
+Date: 2022-12-04 22:42:15
+LastEditTime: 2022-12-04 22:42:16
+Description: 
+'''
